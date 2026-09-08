91674f76320400e7ddc0757722c72b222dd12099
Akilesh kumar S <182787178+Akilesh-kumar-25@users.noreply.github.com>
2026-09-08 20:33:36 +0530

    Fix typo in import statement for firebase_admin.auth

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import json
 import firebase_admin
-from firebase_admin import credentials, firestore, auth
+from firebase_admin import credentials, firestore, aut
 import os
 import socket
 import uuid
